2601e29e451fb73cf196295dd85f195a3a76313b
Sujan <mail.thapasujan@gmail.com>
2024-04-04 10:22:55 +1100

    Homepage done

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,23 @@
-import { useState } from 'react'
+import { useState } from "react";
 import { Navbar } from "./components/Navbar";
 import { Hero } from "./components/Hero";
 import { HomeCards } from "./components/HomeCards";
-import './App.css'
-import { JobListings } from './components/JobListings';
-import { ViewAllJobs } from './components/ViewAllJobs';
+import "./App.css";
+import { JobListings } from "./components/JobListings";
+import { ViewAllJobs } from "./components/ViewAllJobs";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-    <Navbar/>
-    <Hero/>
-    <HomeCards/>
-    <JobListings/>
-    <ViewAllJobs/>
+      <Navbar />
+      <Hero />
+      <HomeCards />
+      <JobListings />
+      <ViewAllJobs />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
