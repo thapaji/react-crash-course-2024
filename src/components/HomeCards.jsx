91b79445e4f5d99455b86cc36290027766a27442
Sujan <mail.thapasujan@gmail.com>
2024-04-04 09:53:20 +1100

    basic react ui conversion done

--- src/components/HomeCards.jsx
+++ src/components/HomeCards.jsx
@@ -5,7 +5,7 @@ export const HomeCards = () => {
   return (
     <section className="py-4">
       <div className="container-xl lg:container m-auto">
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-4 p-4 rounded-lg">
+        <div className="grid grid-cols-1 md :grid-cols-2 gap-4 p-4 rounded-lg">
           <Card>
             <h2 className="text-2xl font-bold">For Developers</h2>
             <p className="mt-2 mb-4">Browse our React jobs and start your career today</p>
