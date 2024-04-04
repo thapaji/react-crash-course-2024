98a8569ea64d35780ad2f4d2e5ff84a6df343406
Sujan <mail.thapasujan@gmail.com>
2024-04-04 12:50:33 +1100

    Jobs page implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,26 @@
-import { useState } from "react";
-import { Navbar } from "./components/Navbar";
-import { Hero } from "./components/Hero";
-import { HomeCards } from "./components/HomeCards";
-import "./App.css";
-import { JobListings } from "./components/JobListings";
 import { ViewAllJobs } from "./components/ViewAllJobs";
+import { NotFoundPage } from "./pages/NotFoundPage";
+import {
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+} from "react-router-dom";
+import { Homepage } from "./pages/Homepage";
+import { MainLayout } from "./layouts/MainLayout";
+import { JobsPage } from "./pages/JobsPage";
 
-function App() {
-  const [count, setCount] = useState(0);
-
-  return (
-    <>
-      <Navbar />
-      <Hero />
-      <HomeCards />
-      <JobListings />
-      <ViewAllJobs />
-    </>
-  );
-}
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<MainLayout />}>
+      <Route index element={<Homepage />} />
+      <Route path="/jobs" element={<JobsPage />} />
+      <Route path="*" element={<NotFoundPage />} />
+    </Route>
+  )
+);
+const App = () => {
+  return <RouterProvider router={router} />;
+};
 
 export default App;
